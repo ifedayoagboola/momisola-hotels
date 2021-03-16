8947b6764f81db9a35176de757ae5592be67839e
ifedayo <ifedayoagboola19@gmail.com>
2021-03-16 16:23:04 +0100

    updating EmailJS keys

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -7,7 +7,12 @@ function Forms() {
     e.preventDefault(); //This is important, i'm not sure why, but the email won't send without it
 
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .sendForm(
+        "service_5s3y8qs",
+        "template_moiktpf",
+        e.target,
+        "user_5SMN4ZHYasmTLuzDkBVy7"
+      )
       .then(
         (result) => {
           window.location.reload(); //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
@@ -22,13 +27,17 @@ function Forms() {
     <form className="contact-form" onSubmit={sendEmail}>
       <input type="hidden" name="contact_number" />
       <label>Name</label>
-      <input type="text" name="from_name" />
+      <input type="text" name="name" />
       <label>Email</label>
-      <input type="email" name="from_email" />
-      <label>Subject</label>
-      <input type="text" name="subject" />
+      <input type="email" name="email" />
+      <label>Phone</label>
+      <input type="text" name="phone" />
+      <label>Date in</label>
+      <input type="text" name="check_in" />
+      <label>Date Out</label>
+      <input type="text" name="check_out" />
       <label>Message</label>
-      <textarea name="html_message" />
+      <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
   );
